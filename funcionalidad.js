1a15864aba3665c56270e0cf9a2b875667bd3d72
valentinamacias <valentinamacias@mariaelvinia.edu.co>
2019-06-05 09:47:52 -0500

    escribiendo comentario

--- funcionalidad.js
+++ funcionalidad.js
@@ -21,7 +21,7 @@ function init() {
     var nueve = document.getElementById('nueve');
     var cero = document.getElementById('cero');
 }
-
+//hola cerdito navideño
 
 uno.onclick = function (e) {
     resultado.textContent = resultado.textContent + "1";
